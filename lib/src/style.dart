54000e2a7d6daaa16178978228cd8086ed024c2a
Thomas Fercher <thomas.fercher@outlook.com>
2023-01-10 11:05:52 +0100

    New Parameter

--- lib/src/style.dart
+++ lib/src/style.dart
@@ -1,9 +1,11 @@
 class LegendStyle {
   const LegendStyle({
     this.nullable = false,
+    this.init = false,
   });
 
   final bool nullable;
+  final bool init;
 }
 
 const legendStyle = LegendStyle();
